71514749346d5bd9b2c618035ec6289a0e91c5ef
teacher <teacher@600900J211DE418>
2018-02-23 11:57:14 +1100

    Created the "GoneHome" Game

--- Assets/~Asteroids/Scripts/Moving.cs
+++ Assets/~Asteroids/Scripts/Moving.cs
@@ -1,64 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Moving : MonoBehaviour
+namespace Asteroids
 {
-    public float rotationSpeed = 360f;
-    public float movementSpeed = 10f;
-
-    
-    void Rotation()
+    public class Moving : MonoBehaviour
     {
-        //Check if left is pressed
-        if (Input.GetKey(KeyCode.LeftArrow))
+        public float rotationSpeed = 360f;
+        public float movementSpeed = 10f;
 
+
+        void Rotation()
         {
-            // Rotate Left
-            transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+            //Check if left is pressed
+            if (Input.GetKey(KeyCode.LeftArrow))
+
+            {
+                // Rotate Left
+                transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+
+            {
+                // Rotate Right
+                transform.Rotate(Vector3.back, rotationSpeed * Time.deltaTime);
+
+
+            }
+        }
+
+
+
+        void Movement()
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+
+
+            {
+                //Vector3 position = transform.position;
+                //position.y += movementSpeed * Time.deltaTime;                                    
+                //transform.position = position;
+                transform.Translate(Vector3.up * movementSpeed * Time.deltaTime);
+            }
+            {
+                print("I have pressed the up key!");
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+
+            {
+                transform.Translate(Vector3.down * movementSpeed * Time.deltaTime);
+
+            }
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            //Call 'Movement()' function
+            Movement();
+            // Call 'Rotation()' function
+            Rotation();
+
 
         }
-        if (Input.GetKey(KeyCode.RightArrow))
-
-        {
-            // Rotate Right
-            transform.Rotate(Vector3.back, rotationSpeed * Time.deltaTime);
-
-
-        }
-    }
-
-
-
-    void Movement()
-    {
-        if (Input.GetKey(KeyCode.UpArrow))
-
-
-        {
-            //Vector3 position = transform.position;
-            //position.y += movementSpeed * Time.deltaTime;                                    
-            //transform.position = position;
-            transform.Translate(Vector3.up * movementSpeed * Time.deltaTime);
-        }
-        {
-            print("I have pressed the up key!");
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-
-        {
-            transform.Translate(Vector3.down * movementSpeed * Time.deltaTime);
-
-        }
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        //Call 'Movement()' function
-        Movement();
-        // Call 'Rotation()' function
-        Rotation();
-
-
     }
 }
