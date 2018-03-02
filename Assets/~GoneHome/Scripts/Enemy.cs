343501491b61516933d0b07a9c17c10ee784dbcb
ElviraEzp <arivle.ezp@gmail.com>
2018-03-02 11:58:20 +1100

    Added Levels and Death

--- Assets/~GoneHome/Scripts/Enemy.cs
+++ Assets/~GoneHome/Scripts/Enemy.cs
@@ -12,16 +12,26 @@ namespace GoneHome
 
         private NavMeshAgent agent;
 
+        private Vector3 spawnPoint;
+
         // Use this for initialization
         void Start()
         {
             agent = GetComponent<NavMeshAgent>();
+
+            spawnPoint = transform.position;
         }
 
         // Update is called once per frame
         void Update()
         {
             agent.SetDestination(target.position);
+        }
+        public void Reset()
+        {
+            agent.enabled = false;
+            transform.position = spawnPoint;
+            agent.enabled = true;
         }
     }
 }
---
+++ Assets/~GoneHome/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+namespace GoneHome
+{
+    public class GameManager : MonoBehaviour
+    {
+        public void NextLevel()
+        {
+            Scene currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.buildIndex + 1);
+        }
+        public void ResetLevel()
+        {
+            //Grab all enemies and reset them
+            Enemy[] enemies = FindObjectsOfType<Enemy>();
+            //Loop through all enemies and reset them
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                enemies[i].Reset();
+            }
+            //Grab the Player and reset it
+            Player player = FindObjectOfType<Player>();
+            player.Reset();
+        }
+        
+    
+    }
+}
