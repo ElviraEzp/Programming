2ce7b4da01bf295b3362a798aa33de895e5c57bc
elvira.kuntoro4 <elvira.kuntoro4@600900J211DE418>
2018-05-25 11:49:18 +1000

    Worked on Asteroids

---
+++ unity/Assets/~Asteroids/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroids
+{
+    public class AsteroidSpawner : MonoBehaviour
+    {
+        public GameObject[] asteroidPrefabs; // Array of prefabs to pawn
+        public float spawnRate = 1f;
+        public float spawnRadius = 5f;
+
+        void Spawn()
+        {
+            // Randomise a position
+            Vector3 rand = Random.insideUnitSphere * spawnRadius;
+            // Cancel the z axis on position
+            rand.z = 0f;
+            Vector3 position = transform.position + rand;
+            int randIndex = Random.Range(0, asteroidPrefabs.Length);
+            GameObject randAsteroid = asteroidPrefabs[randIndex];
+            GameObject clone = Instantiate(randAsteroid);
+            clone.transform.position = position;
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            // Calls a function a specified amount of times
+            InvokeRepeating("Spawn", 0, spawnRate);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
--- unity/Assets/~Asteroids/Scripts/Movement.cs
+++ unity/Assets/~Asteroids/Scripts/Movement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,24 +8,48 @@ namespace Asteroids
 
     public class Movement : MonoBehaviour
     {
-        public float speed = 20f;
-        public float rotationSpeed = 360f;
+        public float speed = 20f; // Units to travel per second
+        public float rotationSpeed = 360f; // Amount of rotation per second
 
-        private RigidBody2D rigid; 
+        private UnityEngine.Rigidbody2D rigid; //Reference to attached RigidBody2D
 
         // Use this for initialization
         void Start()
         {
-            rigid = GetComponent<Rigidbody2D>();
+            rigid = GetComponent<UnityEngine.Rigidbody2D>();
         }
 
         // Update is called once per frame
         void Update()
         {
+            // Check if A key is pressed
             if (Input.GetKey(KeyCode.A))
             {
-                transform.Rotate(Vector3.forward, rotationSpeed);
+                // Rotate left
+                transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
             }
+
+            // Check if D key is pressed
+            if (Input.GetKey(KeyCode.D))
+            {
+                // Rotate right
+                transform.Rotate(Vector3.back, rotationSpeed * Time.deltaTime);
+            }
+
+            // Check if W key is pressed
+            if (Input.GetKey(KeyCode.W))
+            {
+                // Move in facing direction
+                rigid.AddForce(transform.up * speed);
+            }
+        }
+    }
+
+    internal class Rigidbody2D
+    {
+        internal void AddForce(Vector3 vector3, ForceMode2D impulse)
+        {
+            throw new NotImplementedException();
         }
     }
 }
